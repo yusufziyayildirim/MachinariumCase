9528b0bcaddca791f8f5c674b4cf19d1181f595e
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2023-08-21 12:04:38 +0300

    Initial Commit

---
+++ MachinariumCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachinariumCase
+//
+//  Created by Yusuf Ziya YILDIRIM on 21.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
